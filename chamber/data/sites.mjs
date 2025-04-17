8fa87b16d1a79557cf6d353be868614e56724650
chiomaezekwe <chummyankie@gmail.com>
2025-04-17 20:42:28 +0100

    Finished  W05 Chamber Discover Page

---
+++ chamber/data/sites.mjs
@@ -0,0 +1,58 @@
+export const sites =[
+    {
+    "name": "National Theatre",
+    "address": "Iganmu, Surulere, Lagos",
+    "description": "The National Theatre",
+    "detailedDescription": "Built 1976 in preparation for FESTAC 77, is the primary centre for the performing arts in Nigeria. Recently renamed Wole Soyinka centre for Arts and Culture and creative arts.",
+    "image": "images/national-theatre.webp"
+    }, 
+    {
+    "name": "Freedom Park",
+    "address": "Broad Street Lagos",
+    "description": "Freedom Park",
+    "detailedDescription": "The most popular tourist attractions in Lagos. This is due to its rich historical, political, cultural and colonial heritage dating far back as 1882. During its reign, it was one of the most guarded prisons in Nigeria. It holds top criminals and independent agitators.",
+    "image": "images/freedom-park.webp"
+    }, 
+    {
+    "name": "National Stadium",
+    "address": "Surulere, Lagos",
+    "description": "National stadium Lagos",
+    "detailedDescription": "The Lagos National Stadium is a multi-purpose stadium in Surulere, Lagos State, Nigeria. It comprises an Olympic-size swimming arena and a multipurpose arena used for athletics, rugby union, basketball, volleyball, table tennis, wrestling and boxing matches.",
+    "image": "images/national-stadium.webp"
+    }, 
+    {   
+    "name": "National Library",
+    "address": "Herbert Macaulay Way, Alagomeji-Yaba",
+    "description": "National Library of Nigeria, Lagos",
+    "detailedDescription": "In 1948, Nigeria's Standing Committee recommended establishing a National Central Library to store published works, rare materials, and serve as a copyright and microfilm distribution center (White, 1964).",
+    "image": "images/national-library.jpeg"
+    }, 
+    {
+    "name": "Tafawa Balewa Square",
+    "address": "Massey Bamgboshe Street on Lagos Island, Lagos",
+    "description": "Tafawa Balewa Square (TBS)",
+    "detailedDescription": "Tafawa Balewa Square (TBS) in Lagos Island is a 14.5-hectare event venue and transport hub, built in 1972 to commemorate Nigeria’s 1960 independence; formerly called the Race Course.",
+    "image": "images/tbs.jpg"
+    }, 
+    {
+    "name": "The Lekki Conservation Centre",
+    "address": "Lekki-Epe Expressway, Lagos",
+    "description": "Lekki conservation centre",
+    "detailedDescription": "The Lekki Conservation Centre (LCC) is a 78-hectare protected area in Lagos, Nigeria, focused on wildlife and mangrove conservation. It features Africa's longest canopy walkway (401 meters), nature trails, birdwatching, and picnic areas, providing a refuge for diverse species and offering visitors a peaceful natural escape from the urban environment.",
+    "image": "images/lekki-zoo.jpeg"
+    }, 
+    {
+   "name": "Apapa Amusement Park",
+    "address": "Randle Road, Apapa, Lagos",
+    "description": "The Apapa Amusement Park",
+    "detailedDescription": "The Apapa Amusement Park, located in Lagos, Nigeria, covers 7.7 acres. Opened in 2008, it underwent a complete renovation and reopened in 2015 after a three-year closure due to environmental and security concerns.",
+    "image": "images/amusementpark.jpg"
+    }, 
+    {
+    "name": "Eyo Parade Festival",
+    "address": "Lagos Island",
+    "description": "The Eyo Parade Festival",
+    "detailedDescription": "The Eyo Festival, or Adamu Orisha Play, is a Yoruba cultural celebration in Lagos, Nigeria, featuring masquerades dressed in white robes, believed to embody ancestral spirits. It typically takes place on Saturdays.",
+    "image": "images/eyo.webp"
+    }
+];
